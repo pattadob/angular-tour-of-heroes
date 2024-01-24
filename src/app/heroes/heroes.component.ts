f97db67d083a6e648aa4abe6f7b6cb9726423007
Pattadon Bangthip <pattadob@ais.co.th>
2024-01-24 12:43:13 +0700

    Provide HeroService

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css',
+})
+export class HeroesComponent {
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm',
+  // };
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
