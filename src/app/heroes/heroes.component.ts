5a9bfb73c44298f7dac45b6985d9272b2c11f99c
Pattadon Bangthip <pattadob@ais.co.th>
2024-01-26 16:39:32 +0700

    Add a new hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,6 +24,15 @@ export class HeroesComponent {
     this.getHeroes();
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
